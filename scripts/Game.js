b2ecc5be4a4aa78d157b6b7ccaf7e950c9a32233
Armin Raad <45851390+Deltius5kb@users.noreply.github.com>
2023-02-26 18:09:03 +0000

    Refactor

--- scripts/Game.js
+++ scripts/Game.js
@@ -11,23 +11,8 @@ const enum_GameState = {
 export class Game {
     // Level object
     #m_Level;
-    // Threejs PointLight object
-    #m_Light;
-    // Threejs AmbientLight object
-    #m_AmbientLight;
-    // Threejs Scene object
-    #m_Scene;
-    // Threejs Renderer obejct
-    #m_Renderer;
-    // Threejs Camera object
-    #m_Camera;
-    #vec3_CameraPosition;
     #f_DeltaTime;
     #f_TimeAtPreviousFrame;
-    // Helpers 
-    #m_LightHelper;
-    #m_GridHelper;
-    #m_Controls;
 
     // GameScenes
     enum_State;
@@ -35,10 +20,6 @@ export class Game {
     m_SceneMainMenu;
 
     constructor() {
-        this.#SetupScene();
-        this.#SetupRenderer();
-        this.#SetupCamera();   
-        this.#SetupLighting();
         this.#SetupObjects();
         this.#SetupGameScenes();
     }
@@ -49,7 +30,7 @@ export class Game {
     
         switch (this.enum_State) {
             case enum_GameState.Main:
-                this.m_SceneMainMenu.Update();
+                this.m_SceneMainMenu.Update(this.#f_DeltaTime);
                 break;
 
             case enum_GameState.Game:
@@ -60,8 +41,6 @@ export class Game {
 
     // Called every frame from main.js
     Draw() {
-        this.#m_Renderer.render(this.#m_Scene, this.#m_Camera);
-
         switch (this.enum_State) {
             case enum_GameState.Main:
                 this.m_SceneMainMenu.Draw();
@@ -75,14 +54,14 @@ export class Game {
 
     // Called from constructor
     #SetupGameScenes() {
-        this.enum_State = enum_GameState.Main;
+        this.enum_State = enum_GameState.Game;
         
-        let level = 4;
+        let level = 8;
         this.#CreateLevel(level);
         this.#LoadLevel(level);
         
         this.m_SceneMainMenu = new SceneMainMenu();    
-        this.m_SceneGame = new SceneGame(this.#m_Level, this.#m_Scene);
+        this.m_SceneGame = new SceneGame(this.#m_Level);
         
     }
 
@@ -99,59 +78,6 @@ export class Game {
         this.#m_Level = new Level();
     }
     
-    // Called from constructor
-    #SetupLighting() {
-        // Lighting
-        this.#m_Light = new THREE.PointLight(0xbbbbbb);
-        this.#m_Light.position.set(this.#m_Camera.position.x, this.#m_Camera.position.y, this.#m_Camera.position.z + 5);
-        this.#m_AmbientLight = new THREE.AmbientLight(0x909090);
-        this.#m_Scene.add(this.#m_Light, this.#m_AmbientLight);
-    }
-
-    // Run once from constructor
-    #SetupScene() {
-        this.#m_Scene = new THREE.Scene();
-    }
-
-    // Run once from constructor
-    #SetupRenderer() {
-        // Renderer
-        this.#m_Renderer = new THREE.WebGLRenderer({
-            canvas: document.querySelector('#bg'),
-        });
-        this.#m_Renderer.setPixelRatio(window.devicePixelRatio);
-        this.#m_Renderer.setSize(window.innerWidth, window.innerHeight);
-        // Adds renderer to the html document
-        document.body.appendChild(this.#m_Renderer.domElement);
-    }
-
-    // Run once from constructor
-    #SetupCamera() {
-        // https://stackoverflow.com/questions/14614252/how-to-fit-camera-to-object
-        var width = 1920;
-        var dist = 1100;
-        var aspect = 1920 / 1080;
-        // var fov = 2 * Math.atan( height / ( 2 * dist ) ) * ( 180 / Math.PI );
-        var fov = 2 * Math.atan((width / aspect) / (2 * dist)) * (180 / Math.PI) + 7;
-
-        // Camera
-        this.#m_Camera = new THREE.PerspectiveCamera(fov, window.innerWidth / window.innerHeight, 0.1, 10000);
-        this.#vec3_CameraPosition = new THREE.Vector3(960, 620, dist - 200);
-        this.#m_Camera.position.set(this.#vec3_CameraPosition.x, this.#vec3_CameraPosition.y, this.#vec3_CameraPosition.z);
-    }
-
-    // Run once from constructor
-    #SetupHelpers() {
-        // Light helper shows where the light is
-        this.#m_LightHelper = new THREE.PointLightHelper(this.#m_Light);
-        // Draws 2D grid 
-        this.#m_GridHelper = new THREE.GridHelper(200, 50);
-        this.#m_Scene.add(this.#m_LightHelper, this.#m_GridHelper);
-
-        // OrbitControls messes with camera movement, only enable this if necessary
-        this.#m_Controls = new OrbitControls(this.#m_Camera, this.#m_Renderer.domElement);
-    }
- 
     #CreateLevel(level) {
         this.#m_Level.CreateTempLevel(level);
         this.#m_Level.Save(level);
--- scripts/Scene.js
+++ scripts/Scene.js
@@ -1,3 +1,4 @@
+import * as THREE from 'three';
 import { Grid } from './Grid.js'
 import { Bat } from './Bat';
 import { Frame } from './Frame.js';
@@ -6,16 +7,90 @@ import { Timer } from './Timer.js';
 import { ScoreCounter } from './Score.js';
 
 class Scene {
-    constructor() {
+    // Threejs scene
+    _m_Scene;
+    // Threejs Camera
+    _m_Camera;
+    // Threejs PointLight object
+    _m_Light;
+    // Threejs AmbientLight object
+    _m_AmbientLight;
+    // Threejs Renderer obejct
+    _m_Renderer;
+    // Threejs Camera object
+    _vec3_CameraPosition;
 
+    // Helpers 
+    _m_LightHelper;
+    _m_GridHelper;
+    _m_Controls;
+
+    constructor() {
+        this._SetupScene();
+        this._SetupRenderer();
+        this._SetupCamera();   
+        this._SetupLighting();
     }
     
     Update(timeNow) { 
-
+        
     }
 
     Draw() {
+        this._m_Renderer.render(this._m_Scene, this._m_Camera);
+    }
 
+    // Run once from constructor
+    _SetupCamera() {
+        // https://stackoverflow.com/questions/14614252/how-to-fit-camera-to-object
+        var width = 1920;
+        var dist = 1100;
+        var aspect = 1920 / 1080;
+        // var fov = 2 * Math.atan( height / ( 2 * dist ) ) * ( 180 / Math.PI );
+        var fov = 2 * Math.atan((width / aspect) / (2 * dist)) * (180 / Math.PI) + 7;
+
+        // Camera
+        this._m_Camera = new THREE.PerspectiveCamera(fov, window.innerWidth / window.innerHeight, 0.1, 10000);
+        this._vec3_CameraPosition = new THREE.Vector3(960, 620, dist - 200);
+        this._m_Camera.position.set(this._vec3_CameraPosition.x, this._vec3_CameraPosition.y, this._vec3_CameraPosition.z);
+    }
+
+    // Called from constructor
+    _SetupLighting() {
+        // Lighting
+        this._m_Light = new THREE.PointLight(0xbbbbbb);
+        this._m_Light.position.set(this._m_Camera.position.x, this._m_Camera.position.y, this._m_Camera.position.z + 5);
+        this._m_AmbientLight = new THREE.AmbientLight(0x909090);
+        this._m_Scene.add(this._m_Light, this._m_AmbientLight);
+    }
+
+    // Run once from constructor
+    _SetupScene() {
+        this._m_Scene = new THREE.Scene();
+    }
+
+    // Run once from constructor
+    _SetupRenderer() {
+        // Renderer
+        this._m_Renderer = new THREE.WebGLRenderer({
+            canvas: document.querySelector('#bg'),
+        });
+        this._m_Renderer.setPixelRatio(window.devicePixelRatio);
+        this._m_Renderer.setSize(window.innerWidth, window.innerHeight);
+        // Adds renderer to the html document
+        document.body.appendChild(this._m_Renderer.domElement);
+    }
+
+    // Run once from constructor
+    _SetupHelpers() {
+        // Light helper shows where the light is
+        this._m_LightHelper = new THREE.PointLightHelper(this._m_Light);
+        // Draws 2D grid 
+        this._m_GridHelper = new THREE.GridHelper(200, 50);
+        this._m_Scene.add(this._m_LightHelper, this._m_GridHelper);
+
+        // OrbitControls messes with camera movement, only enable this if necessary
+        this._m_Controls = new OrbitControls(this._m_Camera, this._m_Renderer.domElement);
     }
 }
 
@@ -35,6 +110,7 @@ export class SceneMainMenu extends Scene {
 }
 
 export class SceneGame extends Scene {
+    // Threejs Renderer
     // Grid object 
     #m_Grid;
     // Bat object
@@ -49,12 +125,9 @@ export class SceneGame extends Scene {
     #m_Level;
     #f_DeltaTime;
 
-    // Threejs scene object passed in as argument
-    #m_Scene
-
-    constructor(level, scene) {
+    constructor(level) {
         super();
-        this.#m_Scene = scene;
+        this._SetupCamera();
         this.#Initialize(level);
     }
     
@@ -69,19 +142,19 @@ export class SceneGame extends Scene {
         this.#m_Timer.Update(this.#f_DeltaTime);
         this.#m_ScoreCounter.Update();
     }
-    
+
     // Run once from constructor
     #Initialize(level) {
         this.#m_Level = level;
 
-        this.#m_Grid = new Grid(this.#m_Scene);
-        this.#m_Grid.LoadLevel(this.#m_Scene, this.#m_Level);
+        this.#m_Grid = new Grid(this._m_Scene);
+        this.#m_Grid.LoadLevel(this._m_Scene, this.#m_Level);
         
-        this.#m_Bat = new Bat(this.#m_Scene);
-        this.#m_Frame = new Frame(this.#m_Scene);
+        this.#m_Bat = new Bat(this._m_Scene);
+        this.#m_Frame = new Frame(this._m_Scene);
         this.#m_Timer = new Timer("timer");    
         this.#m_ScoreCounter = new ScoreCounter("score");
         
-        this.#m_Ball = new Ball(this.#m_Scene, this.#m_Grid, this.#m_Frame, this.#m_Bat, this.#m_Timer, this.#m_ScoreCounter);
+        this.#m_Ball = new Ball(this._m_Scene, this.#m_Grid, this.#m_Frame, this.#m_Bat, this.#m_Timer, this.#m_ScoreCounter);
     }
 }
